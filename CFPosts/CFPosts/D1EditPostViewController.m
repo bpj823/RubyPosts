bb4928d9c6bcdd6bf7fbc83b6c2b434c9ae1f831
brad <johnson.bradley01@gmail.com>
2013-09-16 22:39:02 -0700

    1st try
    1st try

---
+++ CFPosts/CFPosts/D1EditPostViewController.m
@@ -0,0 +1,68 @@
+//
+//  D1EditPostViewController.m
+//  CFPosts
+//
+//  Created by Brad on 9/11/13.
+//  Copyright (c) 2013 Brad. All rights reserved.
+//
+
+#import "D1EditPostViewController.h"
+
+@interface D1EditPostViewController ()
+
+@property (nonatomic,strong) NSArray *enteredData;
+
+@end
+
+@implementation D1EditPostViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    if (self)
+    {
+        [self receivePostData:self.editPost];
+        
+    }
+        
+    
+}
+
+-(void)receivePostData:(Post *)myPost
+{
+    
+    self.userNameTextField.text = myPost.userName;
+    self.titleTextField.text = myPost.title;
+    self.contentTextField.text = myPost.body;
+    //self.timeStampTextField.text = myPost.timeStamp;
+    
+}
+
+- (IBAction)savePressed:(id)sender
+{
+    
+    _editPost.title =self.titleTextField.text;
+    _editPost.userName =self.userNameTextField.text;
+    _editPost.body =self.contentTextField.text;
+    //editedPost.timeStamp =self.timeStampTextField.text;
+    
+    if([self.delegate respondsToSelector:@selector(saveEditedPost:OnCell:)])
+    {
+        [self.delegate saveEditedPost:_editPost OnCell:self.collectionCellNumber];
+        
+    }
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+}
+
+- (IBAction)timePressed
+{
+    D1TimeDataSourceObject *timeDataSource = [[D1TimeDataSourceObject alloc]init];
+    
+    self.timeStampTextField.text = [NSString stringWithFormat:@" %@", [timeDataSource grabCurrentTime]];
+    
+    
+}
+@end
